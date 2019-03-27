a80c908d5ae5bf9b7877a9fddc1b074f552e3c56
Jannick Joosten <jannick.joosten@gmail.com>
2019-03-27 16:48:07 +0100

    aanpassingen in constraint 3

--- DI Casus/Constraints tests.sql
+++ DI Casus/Constraints tests.sql
@@ -269,21 +269,41 @@ GO
 	Constraint 3
 	All employees should be age 18 or older
 *******************************************************************************************/
-CREATE OR ALTER PROC testEmps.testEmpAge18OrHigherInvalidCase
+EXEC tSQLt.NewTestClass 'testEmployeesAge'
+
+GO
+CREATE OR ALTER PROC testEmployeesAge.SetUp
 AS
 BEGIN
+	EXEC tSQLt.FakeTable 'dbo.emp'
 	EXEC tSQLt.ApplyConstraint 'dbo.emp', 'CHK_employee_age'
-	EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547 -- A check constraint should be violated
-	INSERT INTO emp VALUES (NULL, NULL, NULL, DATEADD(YEAR, -17, GETDATE()), NULL, NULL, NULL, NULL, NULL)
+	SELECT *
+		INTO expected
+		FROM dbo.emp
 END
 GO
 
-CREATE OR ALTER PROC testEmps.testEmpAge18OrHigherValidCase
+CREATE OR ALTER PROC testEmployeesAge.testEmpAge18OrHigherInvalidCase
 AS
 BEGIN
-	EXEC tSQLt.ApplyConstraint 'dbo.emp', 'CHK_employee_age'
+	EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547
+
+	INSERT INTO emp VALUES (NULL, NULL, NULL, DATEADD(YEAR, -17, GETDATE()), NULL, NULL, NULL, NULL, NULL)
+
+	EXEC tSQLt.AssertEqualsTable expected, emp
+END
+GO
+
+CREATE OR ALTER PROC testEmployeesAge.testEmpAge18OrHigherValidCase
+AS
+BEGIN
+	INSERT INTO expected VALUES (NULL, NULL, NULL, DATEADD(YEAR, -18, GETDATE()), NULL, NULL, NULL, NULL, NULL)
+
 	EXEC tSQLt.ExpectNoException
+
 	INSERT INTO emp VALUES (NULL, NULL, NULL, DATEADD(YEAR, -18, GETDATE()), NULL, NULL, NULL, NULL, NULL)
+
+	EXEC tSQLt.AssertEqualsTable expected, emp
 END
 GO
 
